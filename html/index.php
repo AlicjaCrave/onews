77944542cb8fe1969fda90e0a7bcbb9ca1e30432
Alicja Crave <aliciacrave66@gmail.com>
2021-05-18 16:16:46 +0200

    donnes dynamiques ok

---
+++ html/index.php
@@ -0,0 +1,97 @@
+<?php
+require '../inc/header.php';
+?>
+
+<div class="content">
+    <h1>Latest news</h1>
+<div class="articles">
+    <article class="post">
+        <div class="badge badge-news">news</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-john.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="article.php">Continue reading</a>
+    </article>
+
+    <article class="post">
+        <div class="badge badge-team">team</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-dar.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="#">Continue reading</a>
+    </article>
+
+    <article class="post">
+        <div class="badge badge-work">work</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-ohz.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="#">Continue reading</a>
+    </article>
+
+    <article class="post">
+        <div class="badge badge-team">team</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-spr.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="#">Continue reading</a>
+    </article>
+
+    <article class="post">
+        <div class="badge badge-news">news</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-john.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="#">Continue reading</a>
+    </article>
+
+    <article class="post">
+        <div class="badge badge-work">work</div>
+        <div class="post-title">Lorem ipsum dolor sit amet.</div>
+            <div class="data">
+                <img class="data-icon" src="../images/icon-spr.png" alt="John Marchill">
+                <strong class="data-author">John Marchill</strong>
+                <span class="data-date">le 21 Julliet 2019</span>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, alias voluptatum ullam debitis quos
+            voluptates placeat aspernatur deleniti pariatur corporis consectetur commodi omnis velit ab perspiciatis hic
+            natus, qui itaque.</p>
+            <a href="#">Continue reading</a>
+    </article>
+
+
+        </div>
+    </div>
+    </div>
+</body>
+</html>
